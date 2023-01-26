2606999174c0190deebba11abccd0db1c0006e0a
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-26 08:32:20 +0100

    Cleared unused props

--- src/components/ReservationsTop.tsx
+++ src/components/ReservationsTop.tsx
@@ -2,32 +2,23 @@ import * as React from "react";
 import { Box, Container, Title } from "@mantine/core";
 import { BookingForm, SubmitForm } from "src/components";
 import { colors } from "src/theme";
-import { Action } from "src/store/reducers";
 
 interface ReservationsTopProps {
   confirm?: boolean;
-  sending?: boolean;
-  availableTimes: string[];
-  dispatch: React.Dispatch<Action<string>>;
 }
 
-const ReservationsTop: React.FC<ReservationsTopProps> = ({
-  confirm,
-  sending,
-  availableTimes,
-  dispatch,
-}) => {
+const ReservationsTop: React.FC<ReservationsTopProps> = ({ confirm }) => {
   return (
     <Box py="md" sx={{ backgroundColor: colors.primary }}>
       <Container>
         {confirm ? (
-          <SubmitForm sending={sending} dispatch={dispatch} />
+          <SubmitForm />
         ) : (
           <>
             <Title my="sm" c={colors.secondary}>
               Reservations
             </Title>
-            <BookingForm availableTimes={availableTimes} dispatch={dispatch} />
+            <BookingForm />
           </>
         )}
       </Container>

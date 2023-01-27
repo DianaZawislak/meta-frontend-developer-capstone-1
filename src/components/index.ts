f73590816bdf5f01ab4603cd1551242613625487
A. Marzoug Desktop <a.marzoug.pro@gmail.com>
2023-01-27 08:48:04 +0100

    Exported the `Navbar` component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Layout } from "./layout/Layout";
 export { default as Header } from "./layout/Header";
 export { default as Footer } from "./layout/Footer";
+export { default as Navbar } from "./layout/Navbar";
 export { default as Testimonials } from "./Testimonials";
 export { default as Testimonial } from "./Testimonial";
 export { default as Specials } from "./Specials";
